eb9e406eebdbed6a051d7f4b08f21bd1e34fabe1
octavianusfian <octavianus.fian@gmail.com>
2022-10-06 11:54:55 +0700

    add procfile and update ports

--- app.js
+++ app.js
@@ -364,8 +364,13 @@ app.get("/security/:meSubMenu", async function(req, res){
 
 
 // ---------------------------------------
-app.listen(3000, function(){
-    console.log("Server started in port 3000")
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function(){
+    console.log("Server is started")
 });
 
 
